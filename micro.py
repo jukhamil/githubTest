da12e59009bf1ecc237bac85abffadb83e8cbcf3
jukhamil <hamilton.julius@icloud.com>
2020-10-07 23:47:03 +0000

    nothing

---
+++ micro.py
@@ -0,0 +1,2 @@
+#I consider today a major breakthrough. I learned how to write to files with bash, and I learned that coding can be an evening mindless activity - just use the python command line and help function - and in the future every task can be made evening mindless, just make it into a teeny coding command line interface or something that keeps you glued. OK!
+#Ok now I will make my python tool.
--- microstruct.py
+++ microstruct.py
@@ -1,11 +1,2 @@
-print("Enter your to-dos:")
-
-for (string = input() AND string != STOP;  )
-
-
-
-
-#hmmm i really am a hardcore DIYer.... what if i made my own programming language in C in order to make a program? 
-# i.e. for this i define my basic tools: here's how the for loop works - etc.
-
-# OK!!!!!!
+What happens from this?
+What happens from this? This is good
